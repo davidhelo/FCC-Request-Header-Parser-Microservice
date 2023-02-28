562a5e1604ead64b139b93cb0d5e9dfe923f3227
davidhelo <dohernandez93@gmail.com>
2023-02-28 21:34:39 +0000

    feat: ipAddress, language and software response added

--- index.js
+++ index.js
@@ -19,9 +19,17 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+// api/whoami
+app.get('/api/whoami', function (req, res) {
+  //const ipAddress = req.socket.remoteAddress;
+  const ipAddress = req.header('x-forwarded-for');
+  const language = req.header('accept-language');
+  const software = req.header('user-Agent');
+  res.json({
+    "ipaddress": ipAddress,
+    "language": language,
+    "software": software
+  });
 });
 
 // listen for requests :)
